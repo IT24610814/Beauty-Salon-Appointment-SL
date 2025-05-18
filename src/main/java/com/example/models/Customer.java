4e82c383e8b24eb39baa4a441e1cd8ee9c78260a
IT24101177 <it24101177@my.sliit.lk>
2025-05-19 02:38:04 +0530

    Update Customer.java

--- src/main/java/com/example/models/Customer.java
+++ src/main/java/com/example/models/Customer.java
@@ -5,21 +5,24 @@ public class Customer {
     private String name;
     private String email;
     private String phone;
+    private String preferredStylist;
     private String type; // New field for Regular/VIP
 
-    public Customer(int id, String name, String email, String phone) {
+    public Customer(int id, String name, String email, String phone, String preferredStylist) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.preferredStylist = preferredStylist;
         this.type = "Regular"; // Default type
     }
 
-    public Customer(int id, String name, String email, String phone, String type) {
+    public Customer(int id, String name, String email, String phone, String preferredStylist, String type) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.preferredStylist = preferredStylist;
         this.type = type;
     }
 
@@ -53,6 +56,14 @@ public class Customer {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+     public String getPreferredStylist() {
+        return preferredStylist;
+    }
+
+    public void setPreferredStylist(String preferredStylist) {
+        this.preferredStylist = preferredStylist;
     }
 
     public String getType() {
